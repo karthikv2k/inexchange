9b98eb8b627e04a220abcb565994a28ca090c56d
Karthik Ramasamy <karthikv2k@gmail.com>
2012-05-23 03:37:31 -0700

    sync

--- routes/se.js
+++ routes/se.js
@@ -1,8 +1,26 @@
-var https = require('../lib/https_client');
+var se_client = require('../lib/se_client');
+var db = require('../lib/db');
 
 module.exports = function(app){
 
   app.get('/se_auth',function(req, res){
-       https.get({url: 
-      }
+      var code=req.query.code;
+      se_client.get_access_token(code,"http://localhost:5000/se_auth",function(access_token){
+        req.session.se_access_token=access_token;
+        console.log(access_token);
+        se_client.get_id(access_token,function(data){
+          if(data!=null){
+          var id = data['Stack Overflow']; 
+          req.session.se_ids=data;
+          //db.se_update_user(req.session.li_mid,access_token,JSON.stringify(data));
+          se_client.get_questions(id,access_token,function(questions){
+                res.render('nothing.ejs',{module: "se_auth",data:{questions:questions.items}});
+            });
+          }
+          else {
+          res.redirect('/500.html');
+          }
+          });
+        });
+      });
 }
